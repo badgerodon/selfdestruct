6f6b00f5cb6efcacbe13f78dcf281a0addafe1f6
Caleb Doxsey <caleb@doxsey.net>
2013-07-30 10:44:53 -0400

    initial version

---
+++ selfdestruct.go
@@ -0,0 +1,113 @@
+package selfdestruct
+
+import (
+	"sync"
+	"time"
+
+	"github.com/badgerodon/collections/splay"
+	. "github.com/nu7hatch/gouuid"
+)
+
+type (
+	element struct {
+		key     UUID
+		message string
+		expires time.Time
+	}
+	SelfDestructor struct {
+		byKey  *splay.SplayTree
+		byDate *splay.SplayTree
+		lock   sync.Mutex
+	}
+)
+
+func less(a, b UUID) bool {
+	for i := 0; i < 16; i++ {
+		switch {
+		case a[i] < b[i]:
+			return true
+		case a[i] > b[i]:
+			return false
+		}
+	}
+	return false
+}
+
+func New() *SelfDestructor {
+	this := &SelfDestructor{
+		byKey: splay.New(func(a, b interface{}) bool {
+			return less(a.(element).key, b.(element).key)
+		}),
+		byDate: splay.New(func(a, b interface{}) bool {
+			av := a.(element).expires
+			bv := b.(element).expires
+			// in case two keys have identical expire timestamps
+			if av == bv {
+				return less(a.(element).key, b.(element).key)
+			}
+			return bv.After(av)
+		}),
+	}
+	go this.cleaner()
+	return this
+}
+
+func (this *SelfDestructor) cleaner() {
+	for {
+		now := time.Now()
+
+		this.lock.Lock()
+		next := this.byDate.First()
+		if next != nil && now.After(next.(element).expires) {
+			this.byDate.Remove(next)
+			this.byKey.Remove(next)
+		}
+		this.lock.Unlock()
+
+		time.Sleep(time.Second)
+	}
+}
+
+func (this *SelfDestructor) Add(key UUID, message string, expires time.Time) bool {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	el := element{
+		key:     key,
+		message: message,
+		expires: expires,
+	}
+
+	if this.byKey.Has(el) {
+		return false
+	}
+
+	this.byKey.Add(el)
+	this.byDate.Add(el)
+
+	return true
+}
+
+func (this *SelfDestructor) Get(key UUID) (string, bool) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	el, ok := this.byKey.Get(element{key: key}).(element)
+	if !ok {
+		return "", false
+	}
+
+	el, ok = this.byDate.Get(element{key: key, expires: el.expires}).(element)
+	if !ok {
+		return "", false
+	}
+
+	inSixty := time.Now().Add(time.Second * 60)
+	if el.expires.After(inSixty) {
+		el.expires = inSixty
+		this.byKey.Add(el)
+		this.byDate.Add(el)
+	}
+
+	return el.message, true
+}
